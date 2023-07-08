888c152093c08cb00293a0225eeb1e266af8e78e
pattaraphan <124145996+pt139@users.noreply.github.com>
2023-07-08 17:21:54 +0700

    Update script.js

--- css/script.js
+++ css/script.js
@@ -2282,7 +2282,7 @@ const body = document.querySelector("body");
 const a = document.createElement("a");
 a.style.position = "absolute";
 a.style.bottom = "10%";
-a.style.color = "rgb(65, 20, 10)";
+a.style.color = "rgb(43, 12, 6)";
 a.style.textDecoration = "none";
 a.innerText = "CLICK ME!";
 a.setAttribute('target', '_blank');
